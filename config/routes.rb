11ec56c26653c1ee258142be63d42b384fce8c2b
loxigl <43906792+loxigl@users.noreply.github.com>
2023-10-12 07:52:45 +0300

    убрал лишние роуты

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
-  resources :patients, only: [:create, :index, :show], shallow: true do
+  resources :patients, shallow: true do
     resources :recommendations, only: [:index]
   end
 
   resources :consultation_requests, only: [:create], shallow: true do
-    resources :recommendations, only: [:create, :index]
+    resources :recommendations, only: [:create]
 
   end
   get 'drug', to: 'external_api#fetch_one_drug'
